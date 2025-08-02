ea29ec68191a4f25c74ce1b35e204c63ed16c502
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-08-02 08:14:28 +0600

    Add caching in agenda route

--- src/routers/agenda.ts
+++ src/routers/agenda.ts
@@ -1,20 +1,32 @@
-import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { prisma } from "../lib/prisma";
 import { TRPCError } from "@trpc/server";
-import { addDays, startOfDay, endOfDay } from "date-fns";
+import { addDays, endOfDay, startOfDay } from "date-fns";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import redis from "../lib/redis";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const agendaRouter = createTRPCRouter({
   get: protectedProcedure
-    .input(z.object({
-      weekStart: z.string(),
-    }))
+    .input(
+      z.object({
+        weekStart: z.string(),
+      })
+    )
     .query(async ({ ctx, input }) => {
       if (!ctx.user) {
         throw new TRPCError({
           code: "UNAUTHORIZED",
           message: "You must be logged in to get your agenda",
         });
+      }
+
+      const cacheKey = "agenda:get";
+
+      // Try getting data from Redis
+      const cached = await redis.get(cacheKey);
+      console.log("agenda Cache hit:", cached);
+      if (cached) {
+        return JSON.parse(cached);
       }
 
       const weekStart = startOfDay(new Date(input.weekStart));
@@ -69,11 +81,18 @@ export const agendaRouter = createTRPCRouter({
         }),
       ]);
 
-      return {
+      const data = {
         events: {
           personal: personalEvents,
           class: classEvents,
         },
       };
+
+      // Store in Redis (set cache for 10 mins)
+      await redis.set(cacheKey, JSON.stringify(data), {
+        EX: 600, // 600 seconds = 10 minutes
+      });
+
+      return data;
     }),
-}); 
+});

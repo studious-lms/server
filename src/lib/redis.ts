71009750afee1b7dd78e55eef5eac2ff88c3a1b5
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-08-12 19:12:06 +0600

    Fix redis logger issue

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,12 @@
 // lib/redis.ts
 import { createClient } from "redis";
+import { logger } from "src/logger";
 
 const redis = createClient({
   url: process.env.REDIS_URL, // or use host/port config
 });
 
-redis.on("error", (err) => console.error("Redis Client Error", err));
+redis.on("error", (err) => logger.error("Redis Client Error", err));
 
 (async () => {
   if (!redis.isOpen) await redis.connect();

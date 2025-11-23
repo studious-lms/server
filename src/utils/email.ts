d7841546ec8b7727f0117f9f41fc198fe217d104
mesasalata <113854785+mesasalata@users.noreply.github.com>
2025-11-23 15:11:12 +0100

    fix: add email sending placeholder method

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer';
 import { env } from '../lib/config/env.js';
+import { logger } from './logger.js';
+
 
 export const transport = nodemailer.createTransport({
     host: env.EMAIL_HOST,
@@ -10,3 +12,16 @@ export const transport = nodemailer.createTransport({
       pass: env.EMAIL_PASS,
     },
   });
+
+
+export const sendMail = async ({ from, to, subject, text }: { from: string, to: string, subject: string, text: string }) => {
+  // Wrapper function for sending emails
+  // Commented sending email since the system is not setup yet
+  logger.info(`Sending email to ${to} from ${from} with subject ${subject} and text ${text}`);
+  // await transport.sendMail({
+  //   from,
+  //   to,
+  //   subject,
+  //   text,
+  // });
+};
